07483c3baa09b64e106c4c467dbacc17427e4f9e
Uwe Dauernheim <uwe@dauernheim.net>
2013-09-05 16:25:49 +0200

    Initial clojure setup

---
+++ test/infoq_podcast/core_test.clj
@@ -0,0 +1,7 @@
+(ns infoq-podcast.core-test
+  (:require [clojure.test :refer :all]
+            [infoq-podcast.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
