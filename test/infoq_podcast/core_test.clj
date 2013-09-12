4e38298470cba73fc7a04ee5d0f22934dd0e0015
Uwe Dauernheim <uwe@dauernheim.net>
2013-09-12 19:58:39 +0200

    Reset for new branch

--- test/infoq_podcast/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns infoq-podcast.core-test
-  (:require [clojure.test :refer :all]
-            [infoq-podcast.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
