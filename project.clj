d590ad4324e52def808be5a91677408c0bc0045b
Uwe Dauernheim <uwe@dauernheim.net>
2013-10-04 22:36:50 +0200

    Cleanup project file

--- project.clj
+++ project.clj
@@ -4,14 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :uberjar-name "infoq-podcast-standalone.jar"
+  :main infoq-podcast.core
   :dependencies [[clj-http "0.7.7"]
                  [clj-time "0.6.0"]
                  [com.taoensso/timbre "2.6.2"]
                  [enlive "1.1.4"]
                  [hiccup "1.0.4"]
-                 [korma "0.3.0-RC5"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]]
-  :main infoq-podcast.core
-  :uberjar-name "infoq-podcast-standalone.jar")
+                 [org.xerial/sqlite-jdbc "3.7.2"]])
