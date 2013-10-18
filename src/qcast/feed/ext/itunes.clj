b6cfb3e2fb91eb5b190c6a7e66b2ea4fc9b37339
Uwe Dauernheim <uwe@dauernheim.net>
2013-10-18 23:37:18 +0200

    Fix itunes owner

--- src/qcast/feed/ext/itunes.clj
+++ src/qcast/feed/ext/itunes.clj
@@ -20,7 +20,7 @@
 ;; Channel
 
 (defn owner [name email]
-  [:itunes:owner [[:itunes:name name] [:itunes:email email]]])
+  [:itunes:owner '([:itunes:name name] [:itunes:email email])])
 
 (defn categories [& cats]
   (map #(vector :itunes:category {:text (escape-html %)}) cats))
