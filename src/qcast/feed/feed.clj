a7315a34c4312a9e37191d9eaadfce8a0316d2d9
Uwe Dauernheim <uwe@dauernheim.net>
2014-07-24 23:52:03 +0200

    Split feed from server
    Change-Id: I520cce390ae4bea2e68dbba1e393f4269e992e81

---
+++ src/qcast/feed/feed.clj
@@ -0,0 +1,72 @@
+(ns qcast.feed.feed
+  (:require [qcast.feed.ext.atom            :as atom]
+            [qcast.feed.ext.itunes          :as itunes]
+            [qcast.feed.ext.simple-chapters :as psc]
+            [qcast.feed.rss                 :as rss]))
+
+
+;;; Internals
+
+(defn- slides
+  ([] (slides 1))
+  ([n] (cons (str "Slide " n) (lazy-seq (slides (inc n))))))
+
+(defn- feed-item [media-type p]
+  (let [link (:link p)]
+    (remove nil?
+      [(rss/title (:title p))
+       (rss/link link)
+       (rss/description (:summary p))
+       ;;(rss/author (string/join ", " (:authors p)))
+       (rss/author "info@infoq.com (InfoQ)")
+       (rss/pub-date (:publish-date p))
+       (rss/guid link)
+       (when (contains? p media-type) (apply rss/enclosure (media-type p)))
+       (apply rss/categories (:keywords p))
+       ;;(itunes/author (string/join ", " (:authors p)))
+       (itunes/author "info@infoq.com")
+       (itunes/summary (:summary p))
+       ;;(itunes/image (:poster p))
+       (itunes/image (first (:slides p)))
+       (itunes/duration (:length p))
+       (apply psc/chapters (map vector (:times p) (slides) (:slides p)))])))
+
+(defn- feed-channel [change-date]
+  (let [base-url "http://www.infoq.com"
+        title "QCast - InfoQ Presentation Podcast"]
+    [(rss/title title)
+     (rss/link base-url)
+     (rss/description (str "Facilitating the spread of knowledge "
+                           "and innovation in enterprise software "
+                           "development"))
+     (rss/image (str base-url "/styles/i/logo-big.jpg") title base-url)
+     (rss/language "en-US")
+     (rss/generator "InfoQ-Feed-Generator/1.0")
+     (rss/last-build-date change-date)
+     (atom/link "http://infoqcast.herokuapp.com/feed")
+     (itunes/author "InfoQ")
+     (itunes/owner "InfoQ" "info@infoq.com")
+     (itunes/summary (str "InfoQ.com is a practitioner-driven "
+                          "community news site focused on "
+                          "facilitating the spread of knowledge and "
+                          "innovation in enterprise software "
+                          "development."))
+     (itunes/categories "Education" "Technology")
+     (itunes/keywords "Java" ".NET" "dotnet" "Ruby" "SOA"
+                      "Service Oriented Architecture" "Agile"
+                      "enterprise" "software development"
+                      "development" "architecture" "programming")
+     (itunes/image (str base-url "/styles/i/logo-big.jpg"))
+     (itunes/block false)
+     (itunes/explicit false)]))
+
+
+;;; Interface
+
+(defn serve [media data]
+  (let [entries (map :data data)
+        items (map #(feed-item media %) entries)
+        change-date (or (:publish-date (first entries)) 0)
+        channel (feed-channel change-date)
+        extensions [:atom :itunes :simple-chapters]]
+    (rss/feed channel items extensions)))
--- src/qcast/server.clj
+++ src/qcast/server.clj
@@ -1,92 +1,26 @@
 (ns qcast.server
   (:gen-class)
-  (:require [compojure.core                 :refer [defroutes routes GET]]
-            [compojure.handler              :as handler]
-            [compojure.route                :as route]
+  (:require [compojure.core            :refer [defroutes routes GET]]
+            [compojure.handler         :as handler]
+            [compojure.route           :as route]
             [config]
-            [org.httpkit.server             :as http]
-            [qcast.cache                    :as cache]
-            [qcast.feed.ext.atom            :as atom]
-            [qcast.feed.ext.itunes          :as itunes]
-            [qcast.feed.ext.simple-chapters :as psc]
-            [qcast.feed.rss                 :as rss]
-            [qcast.infoq.site               :as infoq]
-            [qcast.ring.middleware.xml      :as xml]
-            [qcast.util                     :refer [parse-int]]
-            [ring.middleware.json           :as json]
-            [ring.util.response             :as response]
-            [taoensso.timbre                :refer :all]))
+            [org.httpkit.server        :as http]
+            [qcast.cache               :as cache]
+            [qcast.feed.feed           :as feed]
+            [qcast.infoq.site          :as infoq]
+            [qcast.ring.middleware.xml :as xml]
+            [qcast.util                :refer [parse-int]]
+            [ring.middleware.json      :as json]
+            [ring.util.response        :as response]
+            [taoensso.timbre           :refer :all]))
 
-;;; Utilities
+
+;;; Internals
 
 (defn wrap-request-logging [handler]
   (fn [{:keys [request-method uri] :as req}]
     (debug request-method uri)
     (handler req)))
-
-
-;;; Internals
-
-(defn- slides
-  ([] (slides 1))
-  ([n] (cons (str "Slide " n) (lazy-seq (slides (inc n))))))
-
-(defn- feed-item [media-type p]
-  (let [link (:link p)]
-    (remove nil?
-      [(rss/title (:title p))
-       (rss/link link)
-       (rss/description (:summary p))
-       ;;(rss/author (string/join ", " (:authors p)))
-       (rss/author "info@infoq.com (InfoQ)")
-       (rss/pub-date (:publish-date p))
-       (rss/guid link)
-       (when (contains? p media-type) (apply rss/enclosure (media-type p)))
-       (apply rss/categories (:keywords p))
-       ;;(itunes/author (string/join ", " (:authors p)))
-       (itunes/author "info@infoq.com")
-       (itunes/summary (:summary p))
-       ;;(itunes/image (:poster p))
-       (itunes/image (first (:slides p)))
-       (itunes/duration (:length p))
-       (apply psc/chapters (map vector (:times p) (slides) (:slides p)))])))
-
-(defn- feed-channel [change-date]
-  (let [base-url "http://www.infoq.com"
-        title "QCast - InfoQ Presentation Podcast"]
-    [(rss/title title)
-     (rss/link base-url)
-     (rss/description (str "Facilitating the spread of knowledge "
-                           "and innovation in enterprise software "
-                           "development"))
-     (rss/image (str base-url "/styles/i/logo-big.jpg") title base-url)
-     (rss/language "en-US")
-     (rss/generator "InfoQ-Feed-Generator/1.0")
-     (rss/last-build-date change-date)
-     (atom/link "http://infoqcast.herokuapp.com/feed")
-     (itunes/author "InfoQ")
-     (itunes/owner "InfoQ" "info@infoq.com")
-     (itunes/summary (str "InfoQ.com is a practitioner-driven "
-                          "community news site focused on "
-                          "facilitating the spread of knowledge and "
-                          "innovation in enterprise software "
-                          "development."))
-     (itunes/categories "Education" "Technology")
-     (itunes/keywords "Java" ".NET" "dotnet" "Ruby" "SOA"
-                      "Service Oriented Architecture" "Agile"
-                      "enterprise" "software development"
-                      "development" "architecture" "programming")
-     (itunes/image (str base-url "/styles/i/logo-big.jpg"))
-     (itunes/block false)
-     (itunes/explicit false)]))
-
-(defn- serve-feed [media]
-  (let [entries (map :data (cache/latest 50))
-        items (map #(feed-item media %) entries)
-        change-date (or (:publish-date (first entries)) 0)
-        channel (feed-channel change-date)
-        extensions [:atom :itunes :simple-chapters]]
-    (rss/feed channel items extensions)))
 
 (defn- respond [body]
   (fn [_req] {:body body}))
@@ -111,8 +45,8 @@
 (defroutes feed-routes
   "Routes for RSS Feeds"
   (GET "/feed"       [] (response/redirect "feed/audio"))
-  (GET "/feed/audio" [] (respond (serve-feed :audio)))
-  (GET "/feed/video" [] (respond (serve-feed :video))))
+  (GET "/feed/audio" [] (respond (feed/serve :audio (cache/latest 50))))
+  (GET "/feed/video" [] (respond (feed/serve :video (cache/latest 50)))))
 
 (defroutes files-routes
   "Routes for file handling and downloads"
