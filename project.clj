1c55d69ed45df890cbdb90c1f3948ee25b8e7cab
Uwe Dauernheim <uwe@dauernheim.net>
2014-07-24 23:05:41 +0200

    Downgrade jdbc
    Somehow the new version is incompatible at least on heroku.
    
    Change-Id: I9cd98e1c68e187eb57ab7fea6be2e7f139b26577

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [http-kit "2.1.16"]
                  [javax.servlet/servlet-api "2.5"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.4"]
+                 [org.clojure/java.jdbc "0.3.0-alpha5"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-json "0.3.1"]])
